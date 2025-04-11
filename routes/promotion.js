f8657115083d423848d6e10bce48d9526903868b
trandoanhuyphuoc19112004 <tdhphuoc22clc@fitus.edu.vn>
2025-04-11 18:07:26 +0700

    [Feat] CRUD in promotions

--- routes/promotion.js
+++ routes/promotion.js
@@ -4,10 +4,10 @@ const promotionController = require('../controller/promotion');
 const { authorizeRole } = require('../middleware/auth');
 
 // ⚠️ Chỉ admin mới được phép
-router.get('/', authorizeRole('admin'), controller.getAllPromotions);
-router.post('/', authorizeRole('admin'), controller.createPromotion);
-router.get('/:id', authorizeRole('admin'), controller.getPromotionById);
-router.put('/:id', authorizeRole('admin'), controller.updatePromotion);
-router.delete('/:id', authorizeRole('admin'), controller.deletePromotion);
+router.get('/', authorizeRole('admin'), promotionController.getAllPromotions);
+router.post('/', authorizeRole('admin'), promotionController.createPromotion);
+router.get('/:id', authorizeRole('admin'), promotionController.getPromotionById);
+router.put('/:id', authorizeRole('admin'), promotionController.updatePromotion);
+router.delete('/:id', authorizeRole('admin'), promotionController.deletePromotion);
 
 module.exports = router;
