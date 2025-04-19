e82cb320a51af50c46a1a830cbecc8d089770fb8
binhtran <tranducbinh2004@gmail.com>
2025-04-19 15:32:42 +0700

    [Enhance] add pagination and sorting to getAllProduct; implement request logging middleware

--- routes/product.js
+++ routes/product.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controller/product');
 
+// Middleware to log the request details
+const requestLogger = (req, res, next) => {
+    console.log('API call received:', {
+      url: req.originalUrl,
+      method: req.method,
+      headers: req.headers,
+      body: req.body,
+      params: req.params,
+      query: req.query
+    });
+    next(); // Important: continue to the next middleware/route handler
+  };
+  
+
+
+// Apply the middleware to all product routes
+router.use(requestLogger);
+
 // Route definitions
 router.get("/", productController.getAllProduct);
 router.get("/:id", productController.getProductById);
 router.post("/", productController.createProduct);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
+router.post("/reset-sequences", productController.resetSequence);
 
 module.exports = router;
